5bc7eb1c9a579de99761658bf16a924ce6a5bcee
SaiChandra2896 <saichandra2896@gmail.com>
2019-07-16 18:29:57 +0530

    add multer and accept files from postman parse the file and send it as json object

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const csv = require('csv-parser');
 const fs = require('fs');
+const multer = require('multer');
+const path = require('path');
 
 
 const app = express();
@@ -11,16 +13,39 @@ app.use(bodyParser.json());
 
 const port = process.env.PORT || 5001;
 
-app.get('/', (req, res) => {
-  let resdata = [];
-  fs.createReadStream('./utils/data.csv')
-    .pipe(csv())
-    .on('data', (data) => {
-      resdata.push(data);
-    })
-    .on('end', () => {
-      res.send(resdata)
-    });
+//set storage engine
+const storage = multer.diskStorage({
+  destination: './public/uploads/',
+  filename: function (req, file, callback) {
+    callback(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+  }
+});
+
+const upload = multer({
+  storage: storage
+}).single('csvfile')
+
+app.post('/', (req, res) => {
+  console.log('abc', req);
+  upload(req, res, (err) => {
+    if (err) {
+      res.json({
+        error: err
+      })
+    }
+    else {
+      console.log('check', req.file);
+      let resdata = [];
+      fs.createReadStream(req.file.destination + req.file.filename)
+        .pipe(csv())
+        .on('data', (data) => {
+          resdata.push(data);
+        })
+        .on('end', () => {
+          res.send(resdata)
+        });
+    }
+  })
 });
 
 app.listen(port, () => {
