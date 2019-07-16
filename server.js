910277b29fc8cdbeb97a1b850884861596021f3e
SaiChandra2896 <saichandra2896@gmail.com>
2019-07-16 15:31:07 +0530

    add boiler plate code to check

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const csv = require('csv-parser');
+const fs = require('fs');
+
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 5001;
+
+app.get('/', (req, res) => {
+  fs.createReadStream('./utils/data.csv')
+    .pipe(csv())
+    .on('data', (row) => {
+      console.log(row);
+    })
+    .on('end', () => {
+      console.log('CSV file successfully processed');
+    });
+});
 
 app.listen(port, () => {
   console.log('Server is up on port ' + port);
