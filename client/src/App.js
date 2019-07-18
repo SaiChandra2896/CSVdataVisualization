926327cc473d949ed0f40e985a209cfc0a7576a3
SaiChandra2896 <saichandra2896@gmail.com>
2019-07-18 18:45:22 +0530

    add graph component

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Fileupload from './components/Fileupload';
 import Table from './components/Table';
@@ -8,17 +7,35 @@ import axios from 'axios';
 
 import './App.css';
 
+const backgroundColor = [
+  'rgba(255, 99, 132, 0.6)',
+  'rgba(54, 162, 235, 0.6)',
+  'rgba(255, 206, 86, 0.6)',
+  'rgba(75, 192, 192, 0.6)',
+  'rgba(153, 102, 255, 0.6)',
+  'rgba(255, 159, 64, 0.6)',
+  'rgba(255, 99, 132, 0.6)'
+]
+
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
       file: {},
       response: [],
+      chartdata: {
+        labels: [],
+        datasets: [
+          {
+            label: 'RupeeValue',
+            data: [],
+          }
+        ]
+      },
       datastate: false,
       errors: {}
     }
   }
-
   handleFileUpload = (e) => {
     e.preventDefault();
     // const data = new FormData();
@@ -42,33 +59,51 @@ class App extends React.Component {
       });
     }
     else {
-      const data = new FormData;
+      const data = new FormData();
       data.append('csvfile', this.state.file, this.state.file.name);
       axios.post('/', data).then((res) => {
+        let dates = [];
+        let values = []
         const resdata = res.data.response;
+        resdata.forEach((e) => {
+          dates.push(e.Date);
+        });
+        resdata.forEach((e) => {
+          values.push(parseInt(e.Price, 10));
+        });
         this.setState({
           response: resdata,
-          datastate: true
+          datastate: true,
+          chartdata: {
+            labels: dates,
+            datasets: [
+              {
+                label: 'RupeeValue',
+                data: values,
+                backgroundColor: backgroundColor
+              }
+            ]
+          }
         })
       }).catch(err => console.log(err));
     }
   }
   render() {
-    console.log('state update', this.state.file);
+    // console.log('state update', this.state.file);
     console.log('response update', this.state.response);
+    console.log('chart data', this.state.chartdata);
     return (
-      <Router>
-        <div className="container mt-4">
-          <h4 className="display-4 text-center mb-4">
-            <i className="fab fa-react"></i> React File Upload
+      <div className="container mt-4">
+        <h4 className="display-4 text-center mb-4">
+          <i className="fab fa-react"></i> React File Upload
             </h4>
-          <Fileupload handleFileUpload={this.handleFileUpload} handleSubmit={this.handleSubmit} />
-          {this.state.datastate ? <Table data={this.state.response} /> : ''}
-          {/* <Route exact path="/upload" component={Fileupload} />
+        <Fileupload handleFileUpload={this.handleFileUpload} handleSubmit={this.handleSubmit} />
+        {this.state.datastate ? <Table data={this.state.response} /> : ''}
+        {this.state.datastate ? <Graph data={this.state.chartdata} Title='Doller to INR plot' /> : ''}
+        {/* <Route exact path="/upload" component={Fileupload} />
           <Route exact path="/table" component={Table} />
           <Route exact path="/table" component={Graph} /> */}
-        </div>
-      </Router>
+      </div>
     );
   }
 }
--- client/src/components/Graph.js
+++ client/src/components/Graph.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
 import { Line } from 'react-chartjs-2';
 
+const style = {
+  marginTop: '5px'
+}
 class Graph extends Component {
   render() {
     return (
       <div className="Graph">
-        Graph Component
+        <Line
+          style={style}
+          data={this.props.data}
+          options={{
+            title: {
+              display: this.props.Title,
+              text: 'Doller to INR comparision',
+              fontSize: 25
+            },
+            legend: {
+              display: true,
+              position: 'bottom',
+              text: 'value'
+            }
+          }}
+        />
       </div>
     )
   }
